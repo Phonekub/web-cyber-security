ab063f474c3f1c93c194b35d5657a7579b400c1e
Supaiwt <phone2546.pp@gmail.com>
2025-10-11 19:58:02 +0700

    Have User email password confirm password and forget password

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,27 +1,38 @@
 import React from "react";
 import "./loginsignup.css";
 import {Card} from "@mui/material"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faUser,faEnvelope,faLock} from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <Card className="container">
       <div className="header">
-        <div>Sign Up</div>
+        <div className="fontheader">Sign Up</div>
         <div className="underline"></div>
       </div>
 
       <div className="inputs">
         <div className="input">
-          <input type="text"/>
+          <FontAwesomeIcon icon={faUser} className="icon"/>
+          <input type="text" placeholder="User" className="text"/>
         </div>
         <div className="input">
-          <input type="email"/>
+          <FontAwesomeIcon icon={faEnvelope}className="icon"/>
+          <input type="email" placeholder="Email" className="text"/>
         </div>
         <div className="input">
-          <input type="password"/>
+          <FontAwesomeIcon icon={faLock}className="icon"/>
+          <input type="password" placeholder="Password" className="text"/>
         </div>
+        <div className="input">
+          <FontAwesomeIcon icon={faLock}className="icon"/>
+          <input type="password" placeholder="Confirm Password" className="text"/>
+        </div>
+        <div className="forget">Forget? <span>Click Here!</span></div>
+        <div>Capcha</div>
         <div className="submit-container">
-          <div>Sign Up</div>
-          <div>Login</div>
+          <div className="submit">Sign Up</div>
+          <div className="submit">Login</div>
         </div>
       </div>
     </Card>
